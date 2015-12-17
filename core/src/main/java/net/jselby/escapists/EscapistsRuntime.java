3b09768c20ea4158438fda80367186fc6a9fda2b
James <jselby@jselby.net>
2015-12-17 23:25:04 +1100

    Enable debugging based on existence of file

--- core/src/main/java/net/jselby/escapists/EscapistsRuntime.java
+++ core/src/main/java/net/jselby/escapists/EscapistsRuntime.java
@@ -29,7 +29,7 @@ import java.util.zip.Inflater;
  */
 public class EscapistsRuntime {
     public static final String VERSION = "0.1";
-    public static final boolean DEBUG = false;
+    public static boolean DEBUG = false;
 
     private static final byte[] UNICODE_GAME_HEADER = "PAMU".getBytes();
     private static final byte[] PACK_HEADER = new byte[]{119, 119, 119, 119, 73, -121, 71, 18};
@@ -80,6 +80,12 @@ public class EscapistsRuntime {
         if (!new File(escapistsDirectory, "TheEscapists_eur.exe").exists()) {
             game.fatalPrompt("Failed to find game's executable within game folder.");
             return false;
+        }
+
+        // Check for debugging
+        if (new File(escapistsDirectory, "debug.flag").exists()) {
+            System.out.println("Debugging ENABLED.");
+            DEBUG = true;
         }
 
         File escapistsFile = new File(escapistsDirectory, "TheEscapists_eur.exe");
