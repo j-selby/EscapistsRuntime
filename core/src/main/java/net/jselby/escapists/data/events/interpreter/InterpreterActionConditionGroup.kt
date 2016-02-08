8be6c8e9a3fc6fb8e8e0907732029d505888aaa4
James <jselby@jselby.net>
2016-02-08 19:46:36 +1100

    [Interpreter] Cleanup scope upon Group completion

--- core/src/main/java/net/jselby/escapists/data/events/interpreter/InterpreterActionConditionGroup.kt
+++ core/src/main/java/net/jselby/escapists/data/events/interpreter/InterpreterActionConditionGroup.kt
@@ -11,83 +11,87 @@ open class InterpreterActionConditionGroup(val group: Events.EventGroup?) {
     open fun invokeIfPossible(interpreter: Interpreter, scope: Scope) {
         group!!
 
-        // Check conditions
-        if (VERBOSE) println("-- Processing condition array: ${group.conditions.toMutableList()}");
-        // Search for OR statement, so we don't instantly short-circuit the result
-        var hasOR = false;
-        for (condition in group.conditions) {
-            if (condition.method.method.name.equals("OrFiltered")) {
-                hasOR = true;
-                break;
-            }
-        }
-
-        var i = 0;
-        while (i < group.conditions.size) {
-            val condition = group.conditions[i];
-            if (condition.method.method.name.equals("OrFiltered")) {
-                if (i == 0) {
-                    throw IllegalStateException("OR token placed at first position?!?!?")
+        try {
+            // Check conditions
+            if (VERBOSE) println("-- Processing condition array: ${group.conditions.toMutableList()}");
+            // Search for OR statement, so we don't instantly short-circuit the result
+            var hasOR = false;
+            for (condition in group.conditions) {
+                if (condition.method.method.name.equals("OrFiltered")) {
+                    hasOR = true;
+                    break;
                 }
-                // If we have successfully made it to this point, we are good to go
-                break;
             }
 
-            var response : Boolean;
-            if (EscapistsRuntime.getRuntime().application.objectDefs.size > condition.objectInfo) {
-                val objectDef = EscapistsRuntime.getRuntime().application.objectDefs[condition.objectInfo];
-                if (objectDef != null) {
-                    response = (interpreter.callMethod(condition.method, condition.items,
-                            condition.identifier, objectDef.handle.toInt()) as Boolean);
+            var i = 0;
+            while (i < group.conditions.size) {
+                val condition = group.conditions[i];
+                if (condition.method.method.name.equals("OrFiltered")) {
+                    if (i == 0) {
+                        throw IllegalStateException("OR token placed at first position?!?!?")
+                    }
+                    // If we have successfully made it to this point, we are good to go
+                    break;
+                }
+
+                var response: Boolean;
+                if (EscapistsRuntime.getRuntime().application.objectDefs.size > condition.objectInfo) {
+                    val objectDef = EscapistsRuntime.getRuntime().application.objectDefs[condition.objectInfo];
+                    if (objectDef != null) {
+                        response = (interpreter.callMethod(condition.method, condition.items,
+                                condition.identifier, objectDef.handle.toInt()) as Boolean);
+                    } else {
+                        response = (interpreter.callMethod(condition.method, condition.items,
+                                condition.identifier) as Boolean);
+                    }
                 } else {
                     response = (interpreter.callMethod(condition.method, condition.items,
                             condition.identifier) as Boolean);
                 }
-            } else {
-                response = (interpreter.callMethod(condition.method, condition.items,
-                        condition.identifier) as Boolean);
-            }
 
-            if (response == condition.inverted()) {
-                if (hasOR) {
-                    // We need to handle this directly
-                    while (i < group.conditions.size) {
-                        if (group.conditions[i].method.method.name.equals("OrFiltered")) {
+                if (response == condition.inverted()) {
+                    if (hasOR) {
+                        // We need to handle this directly
+                        while (i < group.conditions.size) {
+                            if (group.conditions[i].method.method.name.equals("OrFiltered")) {
+                                i++;
+                                break;
+                            }
                             i++;
-                            break;
                         }
-                        i++;
-                    }
 
-                    if (i == group.conditions.size) {
-                        // We reached our last OR statement
+                        if (i == group.conditions.size) {
+                            // We reached our last OR statement
+                            return;
+                        }
+                    } else {
                         return;
                     }
                 } else {
-                    return;
+                    i++;
                 }
-            } else {
-                i++;
-            }
-        }
-
-        if (VERBOSE) println("-- Processing action array: ${group.actions.toMutableList()}");
-        for (action in group.actions) {
-            if (action.method == null) {
-                println("Action $action has no method!");
-                continue;
             }
 
-            if (EscapistsRuntime.getRuntime().application.objectDefs.size > action.objectInfo) {
-                val objectDef = EscapistsRuntime.getRuntime().application.objectDefs[action.objectInfo]
-                if (objectDef != null) {
-                    interpreter.callMethod(action.method, action.items,
-                            action.objectInfo.toShort(), objectDef.handle.toInt())
+            if (VERBOSE) println("-- Processing action array: ${group.actions.toMutableList()}");
+            for (action in group.actions) {
+                if (action.method == null) {
+                    println("Action $action has no method!");
                     continue;
                 }
-            }
 
-            interpreter.callMethod(action.method, action.items, action.objectInfo.toShort())
+                if (EscapistsRuntime.getRuntime().application.objectDefs.size > action.objectInfo) {
+                    val objectDef = EscapistsRuntime.getRuntime().application.objectDefs[action.objectInfo]
+                    if (objectDef != null) {
+                        interpreter.callMethod(action.method, action.items,
+                                action.objectInfo.toShort(), objectDef.handle.toInt())
+                        continue;
+                    }
+                }
+
+                interpreter.callMethod(action.method, action.items, action.objectInfo.toShort())
+            }
+        } finally {
+            interpreter.functions[0].scope.clearScopeObjects();
         }
     }
 
