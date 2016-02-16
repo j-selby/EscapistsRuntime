e5be1c422e8069cdec4c64283027da73e893a5f6
j-selby <jselby@jselby.net>
2016-02-16 12:32:39 +1100

    Change if stack to switch in Comparisons

--- core/src/main/java/net/jselby/escapists/game/events/functions/Comparisons.java
+++ core/src/main/java/net/jselby/escapists/game/events/functions/Comparisons.java
@@ -4,6 +4,7 @@ import net.jselby.escapists.game.ObjectInstance;
 import net.jselby.escapists.game.events.Condition;
 import net.jselby.escapists.game.events.Conditions;
 import net.jselby.escapists.game.events.FunctionCollection;
+import org.apache.commons.lang3.NotImplementedException;
 
 /**
  * Comparison functions, that compare stored values with each other.
@@ -12,34 +13,35 @@ public class Comparisons extends FunctionCollection {
     @Condition(subId = -1, id = -3, conditionRequired = true)
     public boolean Compare(int comparison1, Object str1, int comparison2, Object str2) {
         if (str1 instanceof String) {
-            if (comparison2 == 0) { // EQUAL
-                return str1.equals(str2);
-            } else if (comparison2 == 1) { // DIFFERENT
-                return !str1.equals(str2);
-            } else {
-                throw new IllegalArgumentException("Cannot parse comparsion.");
+            switch (comparison2) {
+                case 0:  // EQUAL
+                    return str1.equals(str2);
+                case 1:  // DIFFERENT
+                    return !str1.equals(str2);
+                default:
+                    throw new IllegalArgumentException("Cannot parse comparsion.");
             }
         }
 
         double num1 = ((Number) str1).doubleValue();
         double num2 = ((Number) str2).doubleValue();
 
-        //System.out.printf("%s=%s?%d.\n", str1.toString(), str2.toString(), comparison2);
         // TODO: comparison1, what does it do?
-        if (comparison2 == 0) { // EQUAL
-            return num1 == num2;
-        } else if (comparison2 == 1) { // DIFFERENT
-            return num1 != num2;
-        } else if (comparison2 == 2) { // LOWER_OR_EQUAL
-            return num1 <= num2;
-        } else if (comparison2 == 3) { // LOWER
-            return num1 < num2;
-        } else if (comparison2 == 4) { // GREATER_OR_EQUAL
-            return num1 >= num2;
-        } else if (comparison2 == 5) { // GREATER
-            return num1 > num2;
-        } else {
-            return false;
+        switch (comparison2) {
+            case 0:  // EQUAL
+                return num1 == num2;
+            case 1:  // DIFFERENT
+                return num1 != num2;
+            case 2:  // LOWER_OR_EQUAL
+                return num1 <= num2;
+            case 3:  // LOWER
+                return num1 < num2;
+            case 4:  // GREATER_OR_EQUAL
+                return num1 >= num2;
+            case 5:  // GREATER
+                return num1 > num2;
+            default:
+                return false;
         }
     }
 
@@ -98,9 +100,11 @@ public class Comparisons extends FunctionCollection {
     }
 
     @Condition(subId = 2, id = -36)
-    public boolean CompareAlterableString(int val1, int vale2) {
-        // TODO: I am so totally screwed
-        return true;
+    public boolean CompareAlterableString(int key, String string) {
+        for (ObjectInstance item : scope.getObjects()) {
+            return string.equals(item.getAlterableStrings()[key]);
+        }
+        return false;
     }
 
     @Condition(subId = 61, id = -27)
@@ -111,10 +115,25 @@ public class Comparisons extends FunctionCollection {
         return false;
     }
 
-    @Condition(subId = 2, id = -42)
-    public boolean CompareAlterableValueInt(int id, int value) {
+    @Condition(subId = 2, id = -42, conditionRequired = true)
+    public boolean CompareAlterableValueInt(int id, int comparisonType, int value) {
         for (ObjectInstance item : scope.getObjects()) {
-            return value == item.getAlterableValues()[id];
+            switch (comparisonType) {
+                case 0: // EQUAL
+                    return value == item.getAlterableValues()[id];
+                case 1: // DIFFERENT
+                    return value != item.getAlterableValues()[id];
+                case 2: // LOWER_OR_EQUAL
+                    return value <= item.getAlterableValues()[id];
+                case 3: // LOWER
+                    return value < item.getAlterableValues()[id];
+                case 4: // GREATER_OR_EQUAL
+                    return value >= item.getAlterableValues()[id];
+                case 5: // GREATER
+                    return value > item.getAlterableValues()[id];
+                default:
+                    return false;
+            }
         }
         return false;
     }
@@ -127,20 +146,21 @@ public class Comparisons extends FunctionCollection {
         }
 
         for (ObjectInstance instance : objects) {
-            if (comparisonType == 0) { // EQUAL
-                return instance.getX() == x;
-            } else if (comparisonType == 1) { // DIFFERENT
-                return instance.getX() != x;
-            } else if (comparisonType == 2) { // LOWER_OR_EQUAL
-                return instance.getX() <= x;
-            } else if (comparisonType == 3) { // LOWER
-                return instance.getX() < x;
-            } else if (comparisonType == 4) { // GREATER_OR_EQUAL
-                return instance.getX() >= x;
-            } else if (comparisonType == 5) { // GREATER
-                return instance.getX() > x;
-            } else {
-                return false;
+            switch (comparisonType) {
+                case 0:  // EQUAL
+                    return instance.getX() == x;
+                case 1:  // DIFFERENT
+                    return instance.getX() != x;
+                case 2:  // LOWER_OR_EQUAL
+                    return instance.getX() <= x;
+                case 3:  // LOWER
+                    return instance.getX() < x;
+                case 4:  // GREATER_OR_EQUAL
+                    return instance.getX() >= x;
+                case 5:  // GREATER
+                    return instance.getX() > x;
+                default:
+                    return false;
             }
         }
         return true;
@@ -154,20 +174,21 @@ public class Comparisons extends FunctionCollection {
         }
 
         for (ObjectInstance instance : objects) {
-            if (comparisonType == 0) { // EQUAL
-                return instance.getY() == y;
-            } else if (comparisonType == 1) { // DIFFERENT
-                return instance.getY() != y;
-            } else if (comparisonType == 2) { // LOWER_OR_EQUAL
-                return instance.getY() <= y;
-            } else if (comparisonType == 3) { // LOWER
-                return instance.getY() < y;
-            } else if (comparisonType == 4) { // GREATER_OR_EQUAL
-                return instance.getY() >= y;
-            } else if (comparisonType == 5) { // GREATER
-                return instance.getY() > y;
-            } else {
-                return false;
+            switch (comparisonType) {
+                case 0:  // EQUAL
+                    return instance.getY() == y;
+                case 1:  // DIFFERENT
+                    return instance.getY() != y;
+                case 2:  // LOWER_OR_EQUAL
+                    return instance.getY() <= y;
+                case 3:  // LOWER
+                    return instance.getY() < y;
+                case 4:  // GREATER_OR_EQUAL
+                    return instance.getY() >= y;
+                case 5:  // GREATER
+                    return instance.getY() > y;
+                default:
+                    return false;
             }
         }
         return true;
@@ -176,6 +197,6 @@ public class Comparisons extends FunctionCollection {
     @Condition(subId = 7, id = -81)
     public boolean CompareCounter(int val1, int val2) {
         // TODO: Compare counters
-        return true;
+        throw new NotImplementedException("CompareCounter:" + val1 + ":" + val2);
     }
 }
